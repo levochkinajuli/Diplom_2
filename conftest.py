9c79ee23ecab8ad17123d3ae03a5d92ebb01259d
Yulia Levochkina <levochkinajuli@yandex.ru>
2024-01-08 12:31:24 +0300

    task1

--- conftest.py
+++ conftest.py
@@ -62,31 +62,3 @@ def token():
     delete_response = requests.delete(delete_url, headers=headers)
 
     assert delete_response.status_code == 202
-
-
-# @pytest.fixture(scope="function")
-# def order():
-#     def generate_random_string(length):
-#         letters = string.ascii_lowercase
-#         random_string = ''.join(random.choice(letters) for i in range(length))
-#         return random_string
-#
-#     email = f'{generate_random_string(4)}@ya.ru'
-#     password = generate_random_string(5)
-#     name = generate_random_string(5)
-#
-#     url = 'https://stellarburgers.nomoreparties.site/api/auth/register'
-#     data = {"email": email, "password": password, "name": name}
-#     response = requests.post(url, json=data)
-#
-#     assert response.status_code == 200
-#
-#     token = response.json().get("accessToken")
-#
-#     yield token
-#
-#     delete_url = 'https://stellarburgers.nomoreparties.site/api/auth/user'
-#     headers = {'Authorization': token}
-#     delete_response = requests.delete(delete_url, headers=headers)
-#
-#     assert delete_response.status_code == 202
